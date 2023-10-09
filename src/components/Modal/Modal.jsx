687c303eeb577361e537f83b8c23e004143dffba
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-10-09 23:24:34 +0300

    fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,25 +5,25 @@ import { ModalOverlay, ModalContent } from './Modal.styled';
 const modalRoot = document.querySelector('#modal-root');
 
 const Modal = ({ handleClose, children }) => {
-  const handleKeyDown = (e) => {
-    if (e.code === 'Escape') {
-      handleClose();
-    }
-  };
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === 'Escape') {
+        handleClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleClose]);
 
   const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
       handleClose();
     }
   };
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
 
   return createPortal(
     <ModalOverlay onClick={handleBackdropClick}>
